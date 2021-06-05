3652c2e39d6d58c9646d96593ef4fa4387607dd7
Felix Boettger <dev@felixboettger.de>
2021-06-05 15:59:55 +0200

    Removed unused face-api nets

--- public/client.js
+++ public/client.js
@@ -20,9 +20,6 @@ webcam.start();
   blazefaceModel = await blazeface.load();
   cocoSsdModel = await cocoSsd.load();
   await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
-  await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
-  await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
-  await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
   await faceapi.nets.faceExpressionNet.loadFromUri("/models");
   await faceapi.nets.ageGenderNet.loadFromUri("/models");
 })().then(function (){
